b56cd8e8f2072956aea4707fead82cf71effbadf
info1 <1422509@cegepoutaouias.qc.ca>
2025-09-29 09:23:40 -0400

    aaaa

--- psedocode_ZIGOMAR.py
+++ psedocode_ZIGOMAR.py
@@ -34,12 +34,12 @@ choix de selection d'objet a retirer du sac en cas de trop pleins d'objets:
 rappel: AUTO TOUT OBJETS QUI COMMENCE PAR B OU CONTIENS UN CHIFFRE. else OUR CHOICE.
 donc:
 
-PSEUDOCODE A LA BOURRE:
+PPSEUDOCODE A LA BOURRE:
 
 def mini_jeu():
     print({liste des objets dans sac})
     liste d'objets.random.randint:
-    print({oobjet a indice random})
+    print({objet a indice random})
 
 def zigomar(mini_jeu, sac, liste d'objets):
     if mini_jeu==False:
@@ -47,7 +47,37 @@ def zigomar(mini_jeu, sac, liste d'objets):
     elif mini_jeu == True:
     zigomar == :)
 
+    afficher ls_inventaire
+    afficher dialogue('voyons voir nos précieux items!')
+    >if inventaire >15:
+inventaire for in range(quantitée necessaire a retirer) (???) parceque si on decide vraiment de faire un nombre random faut coder une limite impenetrable.
+    afficher dialogue ('MON SAC EST PLEINS! vite! qu'on retire un item! J'AI PAS FINI DE PILLER! >:D ')
+    si objet count('B','b'): ON PEUT AUSSI UTLISER ---> e in liste : retourne True si l'élément e appartient à la liste liste, ou retourne False sinon.
+ ls_inventaie= remove(boulles)
+ else input('BAH! toi choisis! je ne peux pas me décider!!)< la on rentrerais le nom de l'item a retirer ou le nombre de
+                                                            l'indice parceque faut pas écrire 'squelette' et tout les 4 squelettes (exemple) sois retirer.
+on peut aussi faire afficher le random integer de chaques indices de la liste d'inventaire, comme sa Zigomar peux retirer 1 objets de la liste selon son program.
+
+
 def inventaire:
+invetaire égal [randomSac(objet_dans_sac)                # Randomiser le sac]
+
+so:
+
+[randomChoice                            # C'est pour choisir un objet random pour le mini-quiz]
+       [ random.choice(objet_dans_sac)]
+
+ls_inventaire.randomchoice:
+potions.random.randint , clees.random.randint, boulles.random.randint  et squelettes.random.randint
+exemple:
+ls_inventaire(3 potions, 3 clees, 3 boulles, 3 squelettes) [=12 items]
+print({random choice}) ###est-ce qu'on donne des guess a zigomar? 3 chances genre? a revoir
+zigomar = random choice
+if zigomar.random.choice = random.choice:
+mini_jeu = True
+print('HAHA JE LE SAVAIS! >:D )
+esle:
+print('tu as tricher! je suis sure que j'avais raison! >:( ')
 
 
 '''
