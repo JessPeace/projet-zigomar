39c412388943b9228e65fa16c144ba682b0c5122
jessPiece <1422509@cegepoutaouais.qc.ca>
2025-10-01 07:55:25 -0400

    IT'S STILL ME(JESS) d'un ordi d'école

--- code_ZIGOMAR.py
+++ code_ZIGOMAR.py
@@ -1,62 +1,106 @@
 #Jonathan et Jessyka (oui j'ai utilisée l'accompte a mon copain)
 
 import random
+from os import remove
 
-def newDay(objets_dans_sac, new_object):                       # Fonction pour la nouvelle journée
-    print("test")
 
-def sac_plein(objet_dans_sac, objet_a_mettre_d_sac):
+def newDay(objets_dans_sac):                       # Fonction pour la nouvelle journée
+    random_nb =  random.randrange(1, 3)
+    for i in range (random_nb):
+        random_items = random.randint(1, 3)
+        for i in range (random_items):
+            objets_dans_sac.append("potions scintillantes")
+        random_items = random.randint(1, 3)
+        for i in range (random_items):
+            objets_dans_sac.append("clés mystérieuses")
+        random_items = random.randint(1, 3)
+        for i in range (random_items):
+            objets_dans_sac.append("boules brillantes")
+        random_items = random.randint(1, 3)
+        for i in range (random_items):
+            objets_dans_sac.append("squelettes miniatures")
+    text_inventaire(objets_dans_sac)
+    print("debut de la nouvelle journée: \n", ls_inventaire)
+    print("allons a l'aventure!")
+
+
+def sac_plein(objets_dans_sac):
+    print("Mon sac est plein!")
     random_nb =  random.randint(0, 1)
     if random_nb == 1:
-        for i in range (len_liste):
-            if objets_dans_sac[i].find("B", "b"):
+        for i in range (len(objets_dans_sac)):
+            text_inventaire(objets_dans_sac)
+            print(ls_inventaire)
+            if objets_dans_sac[i].find("boules brillantes"):
                 objets_dans_sac.pop(i)
-        i += 1
+                print("on enlève cet objet abjecte!")
     else:
-        print(text_inventaire)
         while True:
+            text_inventaire(objets_dans_sac)
+            print(ls_inventaire)
             objet_enlever = str(input("Bah, toi choisis,j'arrive pas à me décider!"))
-            if objet_enlever is str:
+            try:
+                objet_enlever is str
                 if objet_enlever in objets_dans_sac:
+                    objets_dans_sac.remove(objet_enlever)
                     print("on enlève ", objet_enlever, " alors!")
                     break
                 else:
                     print("C’est pas dans mon sac ça!")
-            else:
+            except:
                 print(" ")
 
 
 def randomSac(objet_dans_sac):                # Randomiser le sac
     random.shuffle(objet_dans_sac)
 
-def randomChoice(objet_dans_sac):             # C'est pour choisir un objet random pour le mini-quiz
-    choice_random = random.choice(objet_dans_sac) # Choix random pour Mini-quiz
-    choice_zigomar = random.choice(objet_dans_sac)# Choix zigomar pour mini-quiz
+def randomChoice(objet_dans_sac):                       # C'est pour choisir un objet random pour le mini-quiz
+    choice_random = random.choice(objet_dans_sac)       # Choix random pour Mini-quiz
+    choice_zigomar = random.choice(objet_dans_sac)      # Choix zigomar pour mini-quiz
     if choice_random != choice_zigomar:
         print("Tu as triché! Je suis sûre que je l'avais!")
     else:
         print("AHa! Je le savais! Mes sens d'aventurière ne me mentent jamais!")
 
-def start_sac(random_items, objets_dans_sac):
+def start_sac(objets_dans_sac, ls_inventaire):
     random_items = random.randint(0, 3)
     for i in range (random_items):
         objets_dans_sac.append("potions scintillantes")
-    ls_inventaire.append(str(random_items, " potions scintillante, "))
+    ls_inventaire.append(f"{random_items} potions scintillante")
     random_items = random.randint(0, 3)
     for i in range (random_items):
         objets_dans_sac.append("clés mystérieuses")
-    ls_inventaire += str(random_items, " clés mystérieuses, ")
+    ls_inventaire.append(f"{random_items} clés mystérieuses")
     random_items = random.randint(0, 3)
     for i in range (random_items):
         objets_dans_sac.append("boules brillantes")
-    ls_inventaire += str(random_items, " boules brillantes, ")
+    ls_inventaire.append(f"{random_items} boules brillantes")
     random_items = random.randint(0, 3)
     for i in range (random_items):
         objets_dans_sac.append("squelettes miniatures")
-    ls_inventaire += str(random_items, " boules brillantes")
+    ls_inventaire.append(f"{random_items} squelettes miniatures")
 
-def text_inventaire() :                      # Convertion du sac en texte genre 3 potions, instead of ["potion", "potion", "potion"]
-    print("test")
+def text_inventaire(objets_dans_sac) :                       # Convertion du sac en texte genre 3 potions, instead of ["potion", "potion", "potion"]
+    ls_inventaire = []
+
+    inventaire_en_texte = objets_dans_sac.count("potions scintillantes")
+    ls_inventaire.append(f"{inventaire_en_texte} potions scintillante")
+
+    inventaire_en_texte = objets_dans_sac.count("clés mystérieuses")
+    ls_inventaire.append(f"{inventaire_en_texte} clés mystérieuses")
+
+    inventaire_en_texte = objets_dans_sac.count("boules brillantes")
+    ls_inventaire.append(f"{inventaire_en_texte} boules brillantes")
+
+    inventaire_en_texte = objets_dans_sac.count("squelettes miniatures")
+    ls_inventaire.append(f"{inventaire_en_texte} squelettes miniatures")
+
+def sort_end_day(objets_dans_sac):
+    print("ALORS! avant de finir cette splendide journée d'aventure, FAUT TRIER MON SAC!")
+    sorted(objets_dans_sac)
+    text_inventaire(objets_dans_sac)
+    print(ls_inventaire)
+
 
 
 #Listes
@@ -67,9 +111,29 @@ ls_nb_items = []                          # C'est le nombre d'items de chaque da
 
 #Variables
 new_object = ""                          # C'est les nouveaux objets qu'elle ramasse dans une nouvelle journée
-random_items = 0                        # Nombre random d'items à ajouté/présent
-random_nb = 0                           # Nombre random
-len_liste = len.objets_dans_sac         # Longueur de l'inventaire
-len_str_liste = objets_dans_sac [i]
+random_items = 0                         # Nombre random d'items à ajouté/présent
+random_nb = 0                            # Nombre random
+
+if __name__ == '__main__':
+    start_sac(objets_dans_sac, ls_inventaire)
+    for i in range(1):
+        newDay(objets_dans_sac)
+        while len(objets_dans_sac) > 15:
+            sac_plein(objets_dans_sac)
+        text_inventaire(objets_dans_sac)
+        sort_end_day(objets_dans_sac)
+        randomSac(objets_dans_sac)
+        randomChoice(objets_dans_sac)
+
+
+
+
+
+'''  
+
+TODO:faire un desing. embellisement, affichage des liste est du texte.
+
+
+'''
 
 
